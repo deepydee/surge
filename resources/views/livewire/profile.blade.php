3022e714cb83ccac13339d0e9e84a979264646ee
Vitaliy Semenov <synthex@bk.ru>
2022-08-13 22:05:38 +0300

    Showing Upload Previews

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -27,7 +27,12 @@
                 <x-input.group label="Photo" for="photo" :error="$errors->first('newAvatar')">
                     <div class="flex items-center">
                         <span class="h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                            <img src="{{ auth()->user()->avatarUrl() }}" alt="Profile photo">
+                            @if ($newAvatar)
+                                <img src="{{ $newAvatar->temporaryUrl() }}" alt="Profile photo">
+                            @else
+                                <img src="{{ auth()->user()->avatarUrl() }}" alt="Profile photo">
+                            @endif
+                            
                         </span>
                         <span class="ml-5 rounded-md shadow-sm">
                         <input type="file" wire:model='newAvatar'>
