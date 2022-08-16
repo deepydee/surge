1894ba06a128783b1d2f0d496475a318c9633d26
Vitaliy Semenov <synthex@bk.ru>
2022-08-16 20:07:44 +0300

    Basic Sorting

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -9,13 +9,30 @@ use Livewire\WithPagination;
 class Dashboard extends Component
 {
     use WithPagination;
+
     public $search = '';
+    public $sortField;
+    public $sortDirection = 'asc';
+
+    protected $queryString = ['sortField', 'sortDirection'];
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortDirection = $this->sortDirection === 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->sortDirection = 'asc';
+        }
+
+        $this->sortField = $field;
+    }
 
     public function render()
     {
-        sleep(1);
         return view('livewire.dashboard', [
-            'transactions' => Transaction::search('title', $this->search)->paginate(10),
+            'transactions' => Transaction::search('title', $this->search)
+                ->orderBy($this->sortField, $this->sortDirection)
+                ->paginate(10),
         ]);
     }
 }
